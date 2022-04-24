42899a9b5a07e7534c2536e04ab569fa2b4579eb
daviiid99 <daviiideveloper@gmail.com>
2022-04-24 22:17:41 +0200

    msm8916-common : Build android.hardware.radio@1.4

--- lineage_msm8916.mk
+++ lineage_msm8916.mk
@@ -391,6 +391,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0 \
     android.hardware.radio@1.3 \
+    android.hardware.radio@1.4 \
     librmnetctl \
     libcnefeatureconfig \
     libshim_secril \
