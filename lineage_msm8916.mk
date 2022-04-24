119e476ff00a4b90d09ef81249b7baf59db3e030
daviiid99 <daviiideveloper@gmail.com>
2022-04-24 12:32:49 +0200

    msm8916-common : Radio : Update Data config files

--- lineage_msm8916.mk
+++ lineage_msm8916.mk
@@ -411,8 +411,7 @@ PRODUCT_PACKAGES += \
 # Data configuration files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/dsi_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/dsi_config.xml \
-    $(LOCAL_PATH)/configs/data/netmgr_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/netmgr_config.xml \
-    $(LOCAL_PATH)/configs/data/qmi_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/qmi_config.xml
+    $(LOCAL_PATH)/configs/data/netmgr_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/netmgr_config.xml
 
 # Repainter integration
 PRODUCT_PACKAGES += \
